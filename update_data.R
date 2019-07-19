cadea207b891dfc3c3089f44f7cd1ce3887fa0a6
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2019-07-19 12:25:22 +0100

    try to get data table working

--- update_data.R
+++ update_data.R
@@ -3,4 +3,5 @@ person <- sample(c("Robin", "John", "David", "Jane", "Chloe", "Lily", "Sam"), si
 value <- runif(100,0,100)
 df <- data.frame(person =person, dice_roll = dice_roll, value=value)
 
-s3tools::write_df_to_csv_in_s3(df, "alpha-app-rmarkdown-vegawidget-template/random_data.csv", overwrite = TRUE)
+s3tools::write_df_to_csv_in_s3(df, "alpha-app-rmarkdown-vegawidget-template/random_data.csv", overwrite = TRUE, row.names =FALSE)
+
